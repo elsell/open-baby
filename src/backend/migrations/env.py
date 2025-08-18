978552d15df678442a3b09ac78386f54434a0a40
elsell <35787503+elsell@users.noreply.github.com>
2025-08-17 21:34:57 -0400

    refactor(backend): Use app config for migrations db url

--- src/backend/migrations/env.py
+++ src/backend/migrations/env.py
@@ -5,9 +5,13 @@ from sqlalchemy import pool
 
 from alembic import context
 
+from src.config import config as app_config
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+config.set_main_option("sqlalchemy.url", app_config.database_url)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
