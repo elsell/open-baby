41c61e1f0aee9a81b506c2c15ec04742f97d9b85
elsell <35787503+elsell@users.noreply.github.com>
2025-08-17 20:47:40 -0400

    refactor(backend): Configure fastapi auto reload

--- src/backend/entrypoint.sh
+++ src/backend/entrypoint.sh
@@ -8,8 +8,8 @@ fi
 # Run the appropriate command
 if [ "$ENVIRONMENT" = "dev" ]; then
     echo "Running in development mode"
-    /app/venv/bin/fastapi dev main.py --host 0.0.0.0
+    /app/venv/bin/fastapi dev src/main.py --host 0.0.0.0
 else
     echo "Running in production mode"
-    /app/venv/bin/fastapi run main.py --host 0.0.0.0
+    /app/venv/bin/fastapi run src/main.py --host 0.0.0.0
 fi
