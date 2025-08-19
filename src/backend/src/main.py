6484d849040552a302e2d95995fc1aa8053ef23f
elsell <35787503+elsell@users.noreply.github.com>
2025-08-18 23:23:40 -0400

    refactor(backend): Add CORS for local development

--- src/backend/src/main.py
+++ src/backend/src/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
 from events.router import router as events_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # TODO: Make this configurable
+    allow_credentials=True,
+    allow_methods=["GET", "PUT", "PATCH", "DELETE", "POST"],  # Allow all methods
+    allow_headers=["*"],  # Allow all headers
+)
 
 app.include_router(events_router, prefix="/events", tags=["events"])
