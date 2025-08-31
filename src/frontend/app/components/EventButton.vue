9d2b22cbf6bb6f243fa9e77919251f878db06144
elsell <35787503+elsell@users.noreply.github.com>
2025-08-31 16:26:18 -0400

    refactor(frontend): Clearer history on buttons.

--- src/frontend/app/components/EventButton.vue
+++ src/frontend/app/components/EventButton.vue
@@ -1,15 +1,14 @@
 <template>
   <UButton class="h-full w-full cursor-pointer" style="text-transform: capitalize;" @click="$emit('click')">
-    <div class="w-full flex flex-col">
+    <div class="w-full flex flex-col relative">
 
       <div class="w-full h-full flex flex-col items-center justify-center gap-3 text-5xl">
         <UIcon class="text-6xl md:text-9xl" :name="icon" />
         <span class="opacity-80">{{ name }}</span>
       </div>
-      <div class="flex flex-row items-center justify-center gap-3 opacity-50">
+      <div class="flex flex-row items-center justify-center gap-3 text-md absolute top-0 right-0 bg-neutral-300 dark:bg-neutral-800 dark:text-white rounded-md p-1 px-2">
         <UIcon name="i-ph-clock-counter-clockwise" />
         <span>
-          Last {{ name }}
           <NuxtTime v-if="lastEvent" :datetime="lastEvent.time_start"
             :time-zone="Intl.DateTimeFormat().resolvedOptions().timeZone" relative />
         </span>
