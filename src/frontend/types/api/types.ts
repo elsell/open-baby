d4b49bed10b89dce322a5869cf504c9ab6644920
elsell <35787503+elsell@users.noreply.github.com>
2025-09-17 16:13:21 -0400

    chore(frontend): Update API types to include bottle feed stats

--- src/frontend/types/api/types.ts
+++ src/frontend/types/api/types.ts
@@ -264,10 +264,42 @@ export interface paths {
         patch?: never;
         trace?: never;
     };
+    "/stats/feeds": {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        /**
+         * Get Feed Statistic
+         * @description Retrieve a feed statistic by its ID.
+         */
+        get: operations["get_feed_statistic_stats_feeds_get"];
+        put?: never;
+        post?: never;
+        delete?: never;
+        options?: never;
+        head?: never;
+        patch?: never;
+        trace?: never;
+    };
 }
 export type webhooks = Record<string, never>;
 export interface components {
     schemas: {
+        /** BottleFeedStatistic */
+        BottleFeedStatistic: {
+            /**
+             * Time
+             * Format: date-time
+             */
+            time: string;
+            /** Amount Ml */
+            amount_ml: number;
+            /** Time Since Last Feed Minutes */
+            time_since_last_feed_minutes: number;
+        };
         /**
          * BreastSide
          * @description Enum for breast feeding side.
@@ -1294,6 +1326,40 @@ export interface operations {
                 };
                 content: {
                     "application/json": unknown;
+                };
+            };
+            /** @description Validation Error */
+            422: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    "application/json": components["schemas"]["HTTPValidationError"];
+                };
+            };
+        };
+    };
+    get_feed_statistic_stats_feeds_get: {
+        parameters: {
+            query?: {
+                /** @description Start date in ISO 8601 format (YYYY-MM-DD) or a full timestamp for precise filtering */
+                start_date?: string | null;
+                /** @description End date in ISO 8601 format (YYYY-MM-DD) */
+                end_date?: string | null;
+            };
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        requestBody?: never;
+        responses: {
+            /** @description Successful Response */
+            200: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    "application/json": components["schemas"]["BottleFeedStatistic"][];
                 };
             };
             /** @description Validation Error */
